8a30c39e81003e6b70a3fcbeb58325a65377739c
DmAlix <dmalix@yahoo.com>
2022-05-13 14:10:44 +0200

    Make changes to Jwt.Get* functions

--- get_config.go
+++ get_config.go
@@ -1,13 +1,13 @@
 package jwt
 
-func (t *jwt) GetHeaders() *Headers {
-	return &t.config.Headers
+func (t *jwt) GetHeaders() Headers {
+	return t.config.Headers
 }
 
-func (t *jwt) GetClaims() *Claims {
-	return &t.config.Claims
+func (t *jwt) GetClaims() Claims {
+	return t.config.Claims
 }
 
-func (t *jwt) GetParseOptions() *ParseOptions {
-	return &t.config.ParseOptions
+func (t *jwt) GetParseOptions() ParseOptions {
+	return t.config.ParseOptions
 }
